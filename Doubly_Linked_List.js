a51f14122665f37a27870d2ac17a658cd2a2f7ec
Daniel Woldegiorgis <dww2@williams.edu>
2019-03-22 23:03:11 -0400

    Finished DLL

--- Doubly_Linked_List.js
+++ Doubly_Linked_List.js
@@ -64,5 +64,79 @@ class DoublyLinkedList{
         this.length--;
         return oldHead;
     }
-    
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        if(index <= this.length/2){
+            //Working from Start
+            var count = 0;
+            var current = this.head;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+    } else{
+        var count = this.length - 1;
+        var current =  this.tail;
+        while(count !== index){
+            current = current.prev;
+            count--;
+        }
+    }
+    return current;
+    }
+    set(index,val){
+        var foundNode = this.get(index);
+        if(foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index,val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return this.unshift(val);
+        if(index === this.length) return this.push(val);
+        var newNode = new Node(val);
+        var beforeNode = this.get(index-1);
+        var afterNode = beforeNode.next;
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index){
+        if(index < 0 || index >=this.length) return undefined;
+        if(index===0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        var removedNode = this.get(index);
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+        this.length--;
+        return removedNode;
+    }
 }
+
+//BIG-O Analysis
+/*
+1. Insertion - O(1) only add at the end. update single next
+2. Removal - O(1) if removing the head
+           - O(1) if removing the tail
+           - O(1) for other items because never have to traverse entire list for removal
+3. Searching - O(N/2) ~ O(N)
+4. Access - O(N). (No random Access)
+
+IDEAL FOR INSERTION AND DELETION OPERATION.
+However, comes at the cost of having using more memory for the additional previous pointers.
+*/
+var list = new DoublyLinkedList();
+list.push("H");
+list.push("O");
+list.push("M");
+list.push("E");
+
+list.set(1, "o"); //turn lowercase
